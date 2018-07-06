42a836f1b0156c13d920de9b30b877bf1a5566dd
RINO767 <Florian.Lappe@web.de>
2018-07-06 14:03:09 +0200

    fixed hoek vurnability and faulty karma test

--- src/app/app-component/app.component.spec.ts
+++ src/app/app-component/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from '../app-routing.module';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [AppRoutingModule],
       declarations: [
         AppComponent
-      ],
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
