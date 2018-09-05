a55654602cc6b020b9621935adba1017fab3eff6
RINO767 <Florian.Lappe@web.de>
2018-09-05 10:37:48 +0200

    fixed href to work in nginx

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   useRuntimeConfig: true,
-  href: '/ui/',
-  xApiKey: 'CloudiatorUI2018',
-  apiPath: 'http://134.60.64.119:9000'
+  href: '/',
+  xApiKey: '',
+  apiPath: ''
 };
