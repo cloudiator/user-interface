140260f38fdf2bd462894f3bd13f4f7ef7ee83ac
RINO767 <Florian.Lappe@web.de>
2018-09-07 11:06:45 +0200

    fixed broken tests

--- src/app/services/runtime-config.service.spec.ts
+++ src/app/services/runtime-config.service.spec.ts
@@ -1,10 +1,20 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { RuntimeConfigService } from './runtime-config.service';
+import {HttpClientModule} from '@angular/common/http';
+import {combineReducers, StoreModule} from '@ngrx/store';
+import * as fromRoot from '../reducers';
 
 describe('RuntimeConfigService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({
+        ...fromRoot.reducers,
+        'feature': combineReducers(fromRoot.reducers)
+      }),
+        HttpClientModule
+      ],
       providers: [RuntimeConfigService]
     });
   });
