faf149d9af92bfbd3257413815ccca6a2a5f082d
RINO767 <Florian.Lappe@web.de>
2018-09-10 14:13:24 +0200

    added hardware route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import {NgModule} from '@angular/core';
 import {CloudOverviewComponent} from './components/clouds/cloud-overview/cloud-overview.component';
 import {NewCloudComponent} from './components/clouds/new-cloud/new-cloud.component';
 import {CloudViewComponent} from './components/clouds/cloud-view/cloud-view.component';
+import {HardwareOverviewComponent} from './components/hardware/hardware-overview/hardware-overview.component';
 
 const routes: Routes = [
 
   {path: 'cloud/:id', component: CloudViewComponent},
 
   {path: 'new-cloud', component: NewCloudComponent},
+
+  {path: 'hardware', component: HardwareOverviewComponent},
 
   {path: 'clouds', component: CloudOverviewComponent},
   {path: '', component: CloudOverviewComponent},
