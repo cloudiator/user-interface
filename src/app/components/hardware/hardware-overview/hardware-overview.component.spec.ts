f88d49dbbc0e9af7fecf600aa17389d6a4360322
RINO767 <Florian.Lappe@web.de>
2018-09-12 11:40:01 +0200

    fixed tests

--- src/app/components/hardware/hardware-overview/hardware-overview.component.spec.ts
+++ src/app/components/hardware/hardware-overview/hardware-overview.component.spec.ts
@@ -1,6 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HardwareOverviewComponent } from './hardware-overview.component';
+import {HardwareOverviewComponent} from './hardware-overview.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CdkTableModule} from '@angular/cdk/table';
+import {CloudDataService} from '../../../services/cloud-data.service';
+import {CloudService} from 'cloudiator-rest-api';
+import {HttpClientModule} from '@angular/common/http';
+import {combineReducers, StoreModule} from '@ngrx/store';
+import * as fromRoot from '../../../reducers';
+import {DialogService} from '../../../services/dialog.service';
+import {ToastService} from '../../../services/toast.service';
+import {Overlay} from '@angular/cdk/overlay';
+import {Injector} from '@angular/core';
 
 describe('HardwareOverviewComponent', () => {
   let component: HardwareOverviewComponent;
@@ -8,9 +19,27 @@ describe('HardwareOverviewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HardwareOverviewComponent ]
+      declarations: [HardwareOverviewComponent],
+      imports: [
+        StoreModule.forRoot({
+          ...fromRoot.reducers,
+          'feature': combineReducers(fromRoot.reducers)
+        }),
+        FormsModule,
+        ReactiveFormsModule,
+        CdkTableModule,
+        HttpClientModule
+      ],
+      providers: [
+        CloudDataService,
+        CloudService,
+        DialogService,
+        ToastService,
+        Overlay,
+        Injector
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
