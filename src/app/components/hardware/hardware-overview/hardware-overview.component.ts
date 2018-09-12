d70885fae1de93a8d766d4962dfc1df151128207
RINO767 <Florian.Lappe@web.de>
2018-09-12 12:32:44 +0200

    implemented hardware table sort

--- src/app/components/hardware/hardware-overview/hardware-overview.component.ts
+++ src/app/components/hardware/hardware-overview/hardware-overview.component.ts
@@ -15,28 +15,61 @@ export class HardwareOverviewComponent implements OnInit {
 
   searchFormControl = new FormControl();
 
+  sortKey = new BehaviorSubject<string>('');
+  sortDirection = new BehaviorSubject<string>('');
+
   constructor(private cloudDataService: CloudDataService) {
   }
 
   ngOnInit() {
-    combineLatest(this.cloudDataService.findHardware(), this.searchFormControl.valueChanges)
-      .subscribe(([changedHardwareData, searchTerm]) => {
+
+    combineLatest(this.cloudDataService.findHardware(), this.searchFormControl.valueChanges, this.sortKey, this.sortDirection)
+      .subscribe(([changedHardwareData, searchTerm, sortKey, sortDirection]) => {
         const hardwareArray = Object.values(changedHardwareData);
 
+        let filteredHardware = [];
+
         if (!searchTerm) {
-          this.dataSource.next(hardwareArray);
-          return;
+          filteredHardware = hardwareArray;
+        } else {
+          filteredHardware = this.cloudDataService.filterHardware(hardwareArray, searchTerm);
         }
 
-        const filteredResults = this.cloudDataService.filterHardware(hardwareArray, searchTerm);
+        const sortedHardware = filteredHardware.sort((a, b) => {
+          if (a[sortKey] > b[sortKey]) {
+            return sortDirection === 'asc' ? 1 : -1;
+          }
+          if (a[sortKey] < b[sortKey]) {
+            return sortDirection === 'asc' ? -1 : 1;
+          }
+          return 0;
+        });
 
-        this.dataSource.next(filteredResults);
+        this.dataSource.next(sortedHardware);
       });
 
 
     this.cloudDataService.findHardware().subscribe(hardware => {
       this.dataSource = new BehaviorSubject<Hardware[]>(hardware);
+      this.adjustSort('cores');
     });
+
+
+    this.searchFormControl.setValue('');
+  }
+
+  adjustSort(key: string) {
+    if (this.sortKey.value === key) {
+      if (this.sortDirection.value === 'asc') {
+        this.sortDirection.next('desc');
+      } else {
+        this.sortDirection.next('asc');
+      }
+      return;
+    }
+
+    this.sortKey.next(key);
+    this.sortDirection.next('asc');
   }
 
 }
