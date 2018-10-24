bcc02a76fe00fc9767d6d7d3fababd934e148247
Florian Lappe <florian.lappe@gmail.com>
2018-10-24 06:51:26 +0200

    implemented basic yaml editor file down-/upload

--- src/app/components/editor/yaml-editor/yaml-editor.component.ts
+++ src/app/components/editor/yaml-editor/yaml-editor.component.ts
@@ -3,6 +3,7 @@ import * as ace from 'brace';
 import 'brace/mode/yaml';
 import 'brace/theme/monokai';
 import {Editor} from 'brace';
+import saveAs from 'file-saver';
 
 @Component({
   selector: 'app-yaml-editor',
@@ -43,5 +44,26 @@ export class YamlEditorComponent implements OnInit {
       '- node'
     ].join('\n'));
     this.editor.clearSelection();
+  }
+
+  download() {
+    const filename = 'task.yaml';
+
+    const blob = new Blob([this.editor.getValue()], {
+      type: 'text/plain;charset=utf-8'
+    });
+
+    saveAs(blob, filename);
+  }
+
+  onUploadChange(event) {
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      reader.readAsBinaryString(file);
+      reader.onload = () => {
+        this.editor.setValue(reader.result);
+      };
+    }
   }
 }
